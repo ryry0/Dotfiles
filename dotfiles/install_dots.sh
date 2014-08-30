2e00c4f866d34036e6cd0fafe2a1856091ce4725
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-08-30 14:54:58 -0400

    First implementation of dotfile installer.
    Run this from the dotfiles directory. It will link all listed dotfiles
    to the home directory.

---
+++ dotfiles/install_dots.sh
@@ -0,0 +1,9 @@
+dir=~/Dropbox/Programming/usr/dotfiles # dotfiles directory olddir=~/dotfiles_old             # old dotfiles backup directory
+files=".bashrc .vimrc .vim .Xresources .crawlrc .fehbg .ryry.zsh-theme .spectrwm.conf .tmux.conf .xinitrc .zshalias"        # list of files/folders to symlink in homedir
+
+for file in $files; do
+    #echo "Moving any existing dotfiles from ~ to $olddir"
+    #mv ~/$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
+done
