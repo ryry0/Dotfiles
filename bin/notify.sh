91888dd0e11c1cbe3227e6a8424d738c69af70cf
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-09-01 22:01:27 -0400

    First implementation of notification system.
    This system uses Lemonboy's bar. It is a script wrapper that makes it
    easy to implement popup notifications.

---
+++ bin/notify.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# how long should the popup remain?
+duration=5
+
+# define geometry
+barx=10
+bary=10
+#barw=60
+barh=20
+
+# colors
+bar_bg='#ff333333'
+bar_fg='#ffffffff' # white is default
+
+# font used
+bar_font=-*-terminus-medium-r-normal--14-*-*-*-*-*-*-*
+#catecho () {
+text=$(
+    if read -t 0; then
+        cat
+    else
+	echo "$@"
+    fi
+); 
+#}
+barw=$(echo " $text" | wc -m);
+barw=$(( $barw * 8));
+#Create the popup 
+(echo " $text"; sleep ${duration}) | 
+bar -g ${barw}x${barh}+${barx}+${bary} -B${bar_bg} -f ${bar_font}
