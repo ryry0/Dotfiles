0d6d6819fb903e520fd78a776a58acdcd585a327
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-09-14 13:23:44 -0400

    Updated the position of the notify script.
    Moved it 10 pixels more in the x and y direction.

--- bin/notify.sh
+++ bin/notify.sh
@@ -4,8 +4,8 @@
 duration=5
 
 # define geometry
-barx=10
-bary=10
+barx=20
+bary=20
 #barw=60
 barh=20
 
@@ -14,7 +14,7 @@ bar_bg='#ff333333'
 bar_fg='#ffffffff' # white is default
 
 # font used
-bar_font=-*-terminus-medium-r-normal--14-*-*-*-*-*-*-*
+bar_font=-*-terminus-medium-r-normal--16-*-*-*-*-*-*-*
 #catecho () {
 text=$(
     if read -t 0; then
