3f580f866cd6607ac6acee5d6059c3166ca40908
Ryan Reyes <ryan.david.reyes@gmail.com>
2016-01-01 11:33:03 -0500

    install_dots.sh now installs vim directories.
    Adds the backup and swap directories.

--- etc/install_dots.sh
+++ etc/install_dots.sh
@@ -13,3 +13,8 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done
+
+echo "Installing vim directories"
+install -dv ~/.temp/bak
+install -dv ~/.temp/swp
+
