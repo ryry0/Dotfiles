d2b200d280f8bb678275c95506c463fdb7d7656f
Ryan Reyes <ryan.david.reyes@gmail.com>
2016-03-09 16:30:55 -0500

    Helper scripts.
    Arch chroot sets up chroot environment for archlinux installed on
    another partition.
    
    Reconnect and wifi.sh are used to connect to the internet via
    wpa_supplicant.

---
+++ bin/arch-chroot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo mount /dev/sda9 /mnt/arch
+
+cd /mnt/arch
+
+sudo mount -t proc proc proc/
+sudo mount --rbind /sys sys/
+sudo mount --rbind /dev dev/
+sudo mount --rbind /run run/
+sudo cp /etc/resolv.conf /mnt/arch/etc/resolv.conf
+
+chroot /mnt/arch /bin/zsh -c exec su ryan
