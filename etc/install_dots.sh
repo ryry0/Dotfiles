dd7309676d75ec670e2c925a98aad8120293ceb4
Ryan Reyes <ryan.david.reyes@gmail.com>
2016-04-05 17:05:54 -0400

    Updated install-dots.sh
    Made sure it doesn't clobber pre-existing files.

--- etc/install_dots.sh
+++ etc/install_dots.sh
@@ -1,15 +1,16 @@
 dir=$(pwd)
-# dotfiles directory olddir=~/dotfiles_old
+# dotfiles directory 
+olddir=~/.dotfiles_old
 # old dotfiles backup directory
 # list of files/folders to symlink in homedir
 files=".bashrc .vimrc .vim .crawlrc \
 .spectrwm.conf .tmux.conf .juliarc.jl \
 .zshalias .zshcomp .zshfunc .zshkeys .zshopts .zshrc .ryry.zsh-theme \
-.xinitrc .Xresources .mirrorboard.xkb .tips"
+.xinitrc .Xresources .mirrorboard.xkb .tips .metacaps.xkb"
 
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    #echo "Moving any existing dotfiles from ~ to $olddir"
-    #mv ~/$file ~/dotfiles_old/
+    mv -v ~/$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done
