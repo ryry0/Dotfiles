7852e5f5837cacc74f8736d174de762a85b0cd02
Ryan Reyes <ryan.david.reyes@gmail.com>
2017-04-29 10:27:47 -0400

    Updated juliarc.
    Easy units.

--- etc/.juliarc.jl
+++ etc/.juliarc.jl
@@ -1,2 +1,8 @@
-using Gadfly
-set_default_plot_size(25cm, 17cm)
+#using Gadfly
+#set_default_plot_size(25cm, 17cm)
+micros = 10.0^-6
+millis = 10.0^-3
+nanos = 10.0^-9
+k = 10.0^3
+megas = 10.0^6
+gigas = 10.0^9
